55770b1dc32f5ec8cc89198fc9b8ed3a06a7a6b4
kaiumuddin <kaium.uddin2909@gmail.com>
2024-03-03 12:45:59 +0600

    Pages created with router setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 export default function App() {
-    return <div>App</div>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} exact />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/me" element={<ProfilePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </>
+    );
 }
